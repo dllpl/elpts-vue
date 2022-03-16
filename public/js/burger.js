b8dae696ecc3f9f2f0532a28e3a2db4778ea815b
Nikita Ivanov <therockyholl@gmail.com>
2022-03-16 07:25:51 +0300

    first commit

---
+++ public/js/burger.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    const burgerBtn = document.querySelector('#burger');
+    const burger__menu = document.querySelector('#menu');
+
+    burgerBtn.addEventListener('click', function () {
+        burgerBtn.classList.toggle('burger-active');
+        if (burgerBtn.classList.contains('burger-active')) {
+            burger__menu.classList.add('burger__menu-active');
+        } else {
+            burger__menu.classList.remove('burger__menu-active');
+        };
+    });
+});
---
+++ src/components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+  <footer class="footer">
+    <div class="footer-top">
+      <a href="#" class="footer__logo link">
+        <img class="" src="img/logo.png" alt="">
+      </a>
+      <span class="footer__title text text--big">Есть вопросы?</span>
+      <button class="btn btn--order">Заказать звонок</button>
+    </div>
+    <div class="footer-bottom">
+      <ul class="footer__list">
+        <li class="footer__item">
+          <a href="" class="footer__link link link--footer">Главная</a>
+        </li>
+        <li class="footer__item">
+          <a href="" class="link link--footer">Сотрудничество</a>
+        </li>
+        <li class="footer__item">
+          <a href="" class="link link--footer">О сервисе</a>
+        </li>
+        <li class="footer__item">
+          <a href="" class="link link--footer">Оформление ЭПТС</a>
+        </li>
+      </ul>
+      <ul class="footer__list">
+        <li class="footer__item">
+          <a href="" class="footer__link link link--footer">Полезное</a>
+        </li>
+        <li class="footer__item">
+          <a href="" class="link link--footer">Политика конфиденциальности</a>
+        </li>
+        <li class="footer__item">
+          <a href="" class="link link--footer">Публичная офера</a>
+        </li>
+      </ul>
+      <ul class="footer__list">
+        <li class="footer__item">
+          <a href="" class="footer__link link link--footer">Помощь</a>
+        </li>
+        <li class="footer__item">
+          <a href="" class="link link--footer">Вопросы и ответы</a>
+        </li>
+        <li class="footer__item">
+          <a href="" class="link link--footer">Контакты</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
