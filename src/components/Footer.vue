99ea4846bb4eb109b6267422b6a7baf4b0aa2d39
Nikita Iv <nick.iv.dev@gmail.com>
2023-07-26 22:46:21 +0300

    Изменен хедер-футер

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,12 +5,15 @@
         <img class="" src="img/logo.png" alt="">
       </a>
       <span class="footer__title text text--big">Есть вопросы?</span>
-      <button class="btn btn--order"><a href="https://wa.me/79869322789" target="_blank">Написать WhatsApp</a></button>
+      <button class="btn btn--order"><a href="https://wa.me/79869322789" target="_blank">Наш WhatsApp</a></button>
     </div>
     <div class="footer-bottom">
       <ul class="footer__list">
         <li class="footer__item">
-          <a href="" class="footer__link link link--footer">Главная</a>
+          <a href="https://gospts.ru/" class="footer__link link link--footer" target="_blank">Главная</a>
+        </li>
+        <li class="footer__item">
+          <a href="https://reg.gospts.ru/?from=gospts#/" class="link link--footer" target="_blank">Оформление ЭПТС</a>
         </li>
         <li class="footer__item">
           <a href="https://gospts.ru/affiliate/" class="link link--footer" target="_blank">Сотрудничество</a>
@@ -19,7 +22,7 @@
           <a href="https://gospts.ru/about/" class="link link--footer" target="_blank">О сервисе</a>
         </li>
         <li class="footer__item">
-          <a href="" class="link link--footer">Оформление ЭПТС</a>
+          <a href="https://gospts.ru/contacts/" class="link link--footer" target="_blank">Контакты</a>
         </li>
       </ul>
       <ul class="footer__list">
@@ -41,7 +44,13 @@
           <a href="https://gospts.ru/about/" class="link link--footer" target="_blank">Вопросы и ответы</a>
         </li>
         <li class="footer__item">
-          <a href="" class="link link--footer">Контакты</a>
+          <a href="tel:89869322789" class="link link--footer"><strong>Приемная: +7(986)932-27-89</strong></a>
+        </li>
+        <li class="footer__item">
+          <a href="tel:89872969111" class="link link--footer"><strong>Техосмотр: +7(987) 296-91-11</strong></a>
+        </li>
+        <li class="footer__item" style="margin-top: 30px">
+          <a href="https://solution-agency.ru/" class="link link--footer" target="_blank">Разработка SOLUTION AGENCY</a>
         </li>
       </ul>
     </div>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,60 +1,90 @@
 <template>
   <header class="header">
-
-    <button id="burger" class="header__burger-button btn" type="">
-      <div class="burger">
-        <span class="line"></span>
-        <span class="line"></span>
-        <span class="line"></span>
+    <div class="header-container" style="display: flex; justify-content: space-between">
+      <div class="header__action-mobile">
+        <a href="" class="pts__action">Получить ЭПТС</a>
+        <a href="tel:79869322789" class="phone__action">
+          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 1408 1408" color="white"><path fill="currentColor" d="M1408 1112q0 27-10 70.5t-21 68.5q-21 50-122 106q-94 51-186 51q-27 0-53-3.5t-57.5-12.5t-47-14.5T856 1357t-49-18q-98-35-175-83q-127-79-264-216T152 776q-48-77-83-175q-3-9-18-49t-20.5-55.5t-14.5-47T3.5 392T0 339q0-92 51-186Q107 52 157 31q25-11 68.5-21T296 0q14 0 21 3q18 6 53 76q11 19 30 54t35 63.5t31 53.5q3 4 17.5 25t21.5 35.5t7 28.5q0 20-28.5 50t-62 55t-62 53t-28.5 46q0 9 5 22.5t8.5 20.5t14 24t11.5 19q76 137 174 235t235 174q2 1 19 11.5t24 14t20.5 8.5t22.5 5q18 0 46-28.5t53-62t55-62t50-28.5q14 0 28.5 7t35.5 21.5t25 17.5q25 15 53.5 31t63.5 35t54 30q70 35 76 53q3 7 3 21z"/></svg>
+        </a>
+        <button id="burger" class="header__burger-button btn" type="">
+          <div class="burger">
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="close"><svg xmlns="http://www.w3.org/2000/svg" height="1.7em" viewBox="0 0 384 512" style="fill: white"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></span>
+          </div>
+        </button>
       </div>
-    </button>
-    <div id="menu" class="burger__menu">
-      <ul class="burger__list ul-reset">
-        <li class="burger__item">
-          <a href="https://gospts.ru/" class="link">Главная</a>
-        </li>
-        <li class="burger__item">
-          <a href="https://gospts.ru/about/" class="link">О системе</a>
-        </li>
-        <li class="burger__item">
-          <a href="" class="link">Оформить заявку</a>
-        </li>
-        <li class="burger__item">
-          <a href="https://gospts.ru/affiliate/" class="link">Сотрудничество</a>
-        </li>
-      </ul>
+
+      <div id="menu" class="burger__menu">
+        <ul class="burger__list ul-reset">
+          <li class="burger__item">
+            <a href="https://gospts.ru/" class="link">Главная</a>
+          </li>
+          <li class="burger__item burger__service-link">
+              <span>Услуги</span>
+              <svg style="margin-left: 0.6em" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
+              <ul class="burger__service-list">
+                <li><a class="service--list-item" href="https://gospts.ru/%d0%bf%d0%be%d0%bb%d1%83%d1%87%d0%b5%d0%bd%d0%b8%d0%b5-%d1%81%d0%b1%d0%ba%d1%82%d1%81/" target="_blank">Получить СБКТС</a></li>
+                <li><a class="service--list-item" href="https://gospts.ru/%d0%bf%d0%b5%d1%80%d0%b5%d1%85%d0%be%d0%b4-%d1%81-%d0%b1%d1%83%d0%bc%d0%b0%d0%b6%d0%bd%d0%be%d0%b3%d0%be-%d0%bf%d1%82%d1%81-%d0%bd%d0%b0-%d1%8d%d0%bf%d1%82%d1%81/" target="_blank">Замена ПТС на ЭПТС</a></li>
+                <li><a class="service--list-item" href="https://gospts.ru/%d0%ba%d1%83%d0%bf%d0%b8%d1%82%d1%8c-%d0%b0%d0%b2%d1%82%d0%be%d0%bc%d0%be%d0%b1%d0%b8%d0%bb%d1%8c-%d0%b8%d0%b7-eac/" target="_blank">Купить авто из ЕАЭС</a></li>
+                <li><a class="service--list-item" href="https://gospts.ru/na-uchet/" target="_blank">Постановка на учет</a></li>
+                <li><a class="service--list-item" href="https://gospts.ru/era-glonass/" target="_blank">ЭРА-ГЛОНАСС</a></li>
+                <li><a class="service--list-item" href="https://gospts.ru/to/" target="_blank">Техосмотр</a></li>
+                <li><a class="service--list-item" href="https://gospts.ru/to-kom/" target="_blank">Техосмотр (коммерческий)</a></li>
+                <li><a class="service--list-item" href="https://gospts.ru/%d1%80%d1%83%d1%81%d1%81%d0%b8%d1%84%d0%b8%d0%ba%d0%b0%d1%86%d0%b8%d1%8f-%d0%b0%d0%b2%d1%82%d0%be%d0%bc%d0%be%d0%b1%d0%b8%d0%bb%d0%b5%d0%b9/" target="_blank">Руссификация авто</a></li>
+                <li><a class="service--list-item" href="https://gospts.ru/reggibdd/" target="_blank">Регистрация изменений ТС</a></li>
+              </ul>
+          </li>
+          <li class="burger__item">
+            <a href="https://gospts.ru/affiliate/" class="link">Сотрудничество</a>
+          </li>
+          <li class="burger__item">
+            <a href="https://gospts.ru/contacts/" class="link" target="_blank">Контакты</a>
+          </li>
+        </ul>
+      </div>
+      <div style="display:inline-flex;">
+        <a href="https://gospts.ru/" class="header__logo link">
+          <img class="" src="img/logo.png" alt="">
+        </a>
+        <nav class="heder__nav nav">
+          <ul class="nav__list">
+            <li class="nav__item">
+              <a href="https://gospts.ru/" class="link link--nav" target="_blank">Главная</a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="link link--nav service--link" style="display: flex; align-items: center">
+                <span>Услуги</span>
+                <svg style="margin-left: 0.6em" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
+                <ul class="service--list">
+                  <li><a class="service--list-item" href="https://gospts.ru/%d0%bf%d0%be%d0%bb%d1%83%d1%87%d0%b5%d0%bd%d0%b8%d0%b5-%d1%81%d0%b1%d0%ba%d1%82%d1%81/" target="_blank">Получить СБКТС</a></li>
+                  <li><a class="service--list-item" href="https://gospts.ru/%d0%bf%d0%b5%d1%80%d0%b5%d1%85%d0%be%d0%b4-%d1%81-%d0%b1%d1%83%d0%bc%d0%b0%d0%b6%d0%bd%d0%be%d0%b3%d0%be-%d0%bf%d1%82%d1%81-%d0%bd%d0%b0-%d1%8d%d0%bf%d1%82%d1%81/">Замена ПТС на ЭПТС</a></li>
+                  <li><a class="service--list-item" href="https://gospts.ru/%d0%ba%d1%83%d0%bf%d0%b8%d1%82%d1%8c-%d0%b0%d0%b2%d1%82%d0%be%d0%bc%d0%be%d0%b1%d0%b8%d0%bb%d1%8c-%d0%b8%d0%b7-eac/" target="_blank">Купить авто из ЕАЭС</a></li>
+                  <li><a class="service--list-item" href="https://gospts.ru/na-uchet/" target="_blank">Постановка на учет</a></li>
+                  <li><a class="service--list-item" href="https://gospts.ru/era-glonass/" target="_blank">ЭРА-ГЛОНАСС</a></li>
+                  <li><a class="service--list-item" href="https://gospts.ru/to/" target="_blank">Техосмотр</a></li>
+                  <li><a class="service--list-item" href="https://gospts.ru/to-kom/" target="_blank">Техосмотр (коммерческий)</a></li>
+                  <li><a class="service--list-item" href="https://gospts.ru/%d1%80%d1%83%d1%81%d1%81%d0%b8%d1%84%d0%b8%d0%ba%d0%b0%d1%86%d0%b8%d1%8f-%d0%b0%d0%b2%d1%82%d0%be%d0%bc%d0%be%d0%b1%d0%b8%d0%bb%d0%b5%d0%b9/" target="_blank">Руссификация авто</a></li>
+                  <li><a class="service--list-item" href="https://gospts.ru/reggibdd/" target="_blank">Регистрация изменений ТС</a></li>
+                </ul>
+              </a>
+            </li>
+            <li class="nav__item">
+              <a href="https://gospts.ru/affiliate/" class="link link--nav" target="_blank">Сотрудничество</a>
+            </li>
+            <li class="nav__item">
+              <a href="https://gospts.ru/contacts/" class="link link--nav" target="_blank">Контакты</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <a href="#" class="header__epts link link--epts"> <a href="tel:79869322789" class="header__phone link">
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 1408 1408"><path fill="currentColor" d="M1408 1112q0 27-10 70.5t-21 68.5q-21 50-122 106q-94 51-186 51q-27 0-53-3.5t-57.5-12.5t-47-14.5T856 1357t-49-18q-98-35-175-83q-127-79-264-216T152 776q-48-77-83-175q-3-9-18-49t-20.5-55.5t-14.5-47T3.5 392T0 339q0-92 51-186Q107 52 157 31q25-11 68.5-21T296 0q14 0 21 3q18 6 53 76q11 19 30 54t35 63.5t31 53.5q3 4 17.5 25t21.5 35.5t7 28.5q0 20-28.5 50t-62 55t-62 53t-28.5 46q0 9 5 22.5t8.5 20.5t14 24t11.5 19q76 137 174 235t235 174q2 1 19 11.5t24 14t20.5 8.5t22.5 5q18 0 46-28.5t53-62t55-62t50-28.5q14 0 28.5 7t35.5 21.5t25 17.5q25 15 53.5 31t63.5 35t54 30q70 35 76 53q3 7 3 21z"/></svg>
+        +7(986)932-27-89</a></a>
     </div>
 
-    <a href="#" class="header__logo link">
-      <img class="" src="img/logo.png" alt="">
-    </a>
-    <nav class="heder__nav nav">
-      <ul class="nav__list">
-        <li class="nav__item">
-          <a href="https://gospts.ru/" class="link link--nav" target="_blank">Главная</a>
-        </li>
-        <li class="nav__item">
-          <a href="https://gospts.ru/about/" class="link link--nav" target="_blank">О системе</a>
-        </li>
-        <li class="nav__item">
-          <a href="#" class="link link--nav">Оформить заявку</a>
-        </li>
-        <li class="nav__item">
-          <a href="https://gospts.ru/affiliate/" class="link link--nav" target="_blank">Сотрудничество</a>
-        </li>
-      </ul>
-    </nav>
-    <a href="tel:88002501201" class="header__phone link">
-      <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-            d="M14.25 7.71875H13.0625C13.062 7.24648 12.8742 6.79368 12.5403 6.45974C12.2063 6.12579 11.7535 5.93797 11.2812 5.9375V4.75C12.0683 4.75094 12.8229 5.06402 13.3794 5.62057C13.936 6.17711 14.2491 6.93168 14.25 7.71875Z"
-            fill="black"/>
-        <path
-            d="M16.6251 7.71875H15.4376C15.4364 6.61683 14.9981 5.5604 14.2189 4.78123C13.4397 4.00205 12.3833 3.56376 11.2814 3.5625V2.375C12.6981 2.37657 14.0564 2.94008 15.0582 3.94188C16.06 4.94369 16.6235 6.30198 16.6251 7.71875ZM15.4376 17.2188H15.3367C3.66949 16.5478 2.01293 6.70344 1.78136 3.69906C1.74517 3.22807 1.89755 2.76199 2.20498 2.40334C2.51241 2.04469 2.94972 1.82284 3.42071 1.78659C3.46781 1.78303 3.51511 1.78125 3.56261 1.78125H6.69168C6.92951 1.78102 7.16193 1.85221 7.35883 1.9856C7.55574 2.11898 7.70805 2.30842 7.79605 2.52938L8.69855 4.75C8.78545 4.96586 8.80701 5.20248 8.76056 5.43049C8.71412 5.6585 8.60171 5.86784 8.4373 6.0325L7.17261 7.30906C7.36936 8.43204 7.90672 9.46734 8.71181 10.2746C9.51691 11.0818 10.5508 11.6219 11.6732 11.8216L12.9617 10.545C13.1288 10.3824 13.3401 10.2726 13.5692 10.2293C13.7984 10.186 14.0352 10.2112 14.2501 10.3016L16.4886 11.1981C16.7062 11.2889 16.8918 11.4424 17.0219 11.6391C17.152 11.8358 17.2205 12.0667 17.2189 12.3025V15.4375C17.2189 15.9099 17.0312 16.363 16.6971 16.697C16.3631 17.0311 15.91 17.2188 15.4376 17.2188ZM3.56261 2.96875C3.48464 2.96852 3.40738 2.98364 3.33526 3.01326C3.26313 3.04289 3.19755 3.08643 3.14225 3.1414C3.08695 3.19637 3.04301 3.26169 3.01296 3.33364C2.9829 3.40558 2.96732 3.48275 2.96708 3.56072C2.96708 3.57734 2.96768 3.59377 2.96886 3.61C3.24199 7.125 4.99355 15.4375 15.402 16.0313C15.5591 16.0407 15.7136 15.9873 15.8314 15.8828C15.9492 15.7784 16.0207 15.6315 16.0302 15.4743L16.0314 15.4375V12.3025L13.7929 11.4059L12.0889 13.0981L11.8039 13.0625C6.63824 12.4153 5.93761 7.24969 5.93761 7.19625L5.90199 6.91125L7.58824 5.20719L6.69761 2.96875H3.56261Z"
-            fill="black"/>
-      </svg>
-      8 800 250 12 01</a>
-    <a href="#" class="header__epts link link--epts">Оформить ЭПТС</a>
   </header>
 </template>
 
